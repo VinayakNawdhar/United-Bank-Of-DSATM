e4e89c749a0cd1d3089504a9fa35977d6d856a05
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2023-02-28 21:05:26 +0530

    Delete prac.js

--- prac.js
+++
@@ -1,8 +0,0 @@
-const isPalindrom = function(str){
-    const newStr  = [...str];
-    newStr.reverse();
-    return newStr.join("") === str ? true : false;
-}
-
-console.log(isPalindrom("dsjadkl"));
-
